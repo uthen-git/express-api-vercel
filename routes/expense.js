94a93da5f76e220339b4cbb3325f69afc6288c99
uthendevelop <uthendevelop@gmail.com>
2023-06-07 14:54:05 +0700

    เพิ่ม rout หาค่าใช้จ่ายตามเดือน/ปี

--- routes/expense.js
+++ routes/expense.js
@@ -11,6 +11,29 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+router.get('/:year/:month', async (req, res, next) => {
+    const { year, month } = req.params;
+
+    try {
+        const startDate = moment(`${year}-${month}-01`, 'YYYY-MM-DD').startOf('month').toDate();
+        const endDate = moment(`${year}-${month}-01`, 'YYYY-MM-DD').endOf('month').toDate();
+
+        const ExpenseFound = await Expense.aggregate([
+            {
+                $match:{
+                    date:{
+                        $gte:startDate,
+                        $lte:endDate
+                    }
+                }
+            }
+        ])
+        res.json(ExpenseFound);
+    } catch (err) {
+        return next(err);
+    }
+})
+
 router.get('/:id', async (req, res, next) => {
     try {
         const ExpenseFound = await Expense.findById(req.params.id);
